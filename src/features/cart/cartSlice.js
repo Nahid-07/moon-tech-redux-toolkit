9d7960f8dc744f15a0800402d455aa2361946c2b
Nahid-07 <nahidasif1998@gmail.com>
2023-02-04 10:55:22 +0600

    product add to the cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -8,8 +8,17 @@ const cartSlice = createSlice({
     name : "cart",
     initialState,
     reducers :{
-
+        addToCart : (state,action)=>{
+            const selecteProduct = state.cart.find( product => product._id === action.payload._id );
+            if(!selecteProduct){
+                const product = {...action.payload , quantity : 1}
+                state.cart.push(product)
+            }else{
+                selecteProduct.quantity += 1
+                state.cart.filter(product => product._id !== selecteProduct._id).push(selecteProduct)
+            }
+        }
     }
 });
-
+export const {addToCart} = cartSlice.actions
 export default cartSlice.reducer
